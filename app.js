4bf992e13ff0ca534ead2ef55851addfec7726c4
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-09-03 14:28:43 +0800

    rest api addNotification

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const serverPort = config.serverPort;
 const server = require('http').createServer(app);
 const serverUse=require('./lib/serverUse');
 const userRestApi = require("./lib/rest_api/user");
+const notificationRestApi = require("./lib/rest_api/notification");
 
 console.log('config',config)
 
 serverUse.on(app);
 userRestApi.on(app);
+notificationRestApi.on(app);
 
 server.listen(serverPort);
 console.log("現在使用" + serverPort + "port");
